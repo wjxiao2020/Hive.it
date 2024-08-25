6eb408a6ccc05b49617d8fe56ee54f255831d5d6
Weijia <xiao.wei@northeastern.edu>
2024-08-24 21:09:02 -0400

    update firebase configurations

--- firebase.js
+++ firebase.js
@@ -9,13 +9,12 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAV8hdOig3TnsE5DrmHDiNO7nrZNjPw6Go",
-  authDomain: "inventory-management-c41a9.firebaseapp.com",
-  projectId: "inventory-management-c41a9",
-  storageBucket: "inventory-management-c41a9.appspot.com",
-  messagingSenderId: "582130329339",
-  appId: "1:582130329339:web:e4dd9cd468976afd26725e",
-  measurementId: "G-NMXN2QKE7V"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
